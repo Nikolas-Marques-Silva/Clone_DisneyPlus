5d7be2a7f7fafeb9d0d8c58969adceb8e727302b
Níkolas Marques da Silva <nikolas.marques.silva@gmail.com>
2023-12-22 13:44:32 -0300

    feat(modulo_21):header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,22 @@
 const { removeListener } = require("gulp")
 
 document.addEventListener("DOMContentLoaded", function() {
+  // Variáveis
+
   const buttons = document.querySelectorAll("[data-tab-button]")
   const questions = document.querySelectorAll("[data-faq-question]")
+  const heroSection = document.querySelector(".hero")
+  const heroSectionHeight = heroSection.clientHeight
 
+  window.addEventListener("scroll", function() {
+    if (window.scrollY <= heroSectionHeight) {
+      ocultaHeader()
+    } else {
+      mostraHeader()
+    }
+  })
+
+  // Seção de atrações, esconder as abas e mostrar apenas a aba selecionada
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function(botao) {
       const tabTarget = botao.target.dataset.tabButton
@@ -15,10 +28,23 @@ document.addEventListener("DOMContentLoaded", function() {
     })
   }
 
+  // Seção de perguntas(accordion)
   for (let i = 0; i < questions.length; i++) {
     questions[i].addEventListener("click", answerToggle)
   }
 })
+
+// Funções
+
+function mostraHeader() {
+  const header = document.querySelector("header")
+  header.classList.remove("header--is-hidden")
+}
+
+function ocultaHeader() {
+  const header = document.querySelector("header")
+  header.classList.add("header--is-hidden")
+  }
 
 function answerToggle(element) {
   const classe = "faq__questions__item--is-open"
