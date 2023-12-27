17dffd9708f1465b8185312dff05b0fae5af0c4a
Níkolas Marques da Silva <nikolas.marques.silva@gmail.com>
2023-12-27 12:21:07 -0300

    initial commit

--- dist/scripts/main.js
+++
@@ -1,1 +0,0 @@
-const header=document.querySelector("header"),hero=document.querySelector(".hero");window.addEventListener("scroll",()=>{var e=hero.offsetHeight;window.scrollY>e?header.classList.add("header--active"):header.classList.remove("header--active")});
