659e76f5e04da6f56fc50c17e66dbb8c86fa5c2c
Níkolas Marques da Silva <nikolas.marques.silva@gmail.com>
2023-12-28 22:28:48 -0300

    Updates

--- index.fae0640d.js
+++
@@ -1,1 +0,0 @@
-AOS.init();const e=new Date("Jan 09, 2024 00:00:00").getTime(),t=setInterval(()=>{let n=e-new Date().getTime();document.getElementById("contador").innerHTML=`${Math.floor(n/864e5)}d ${Math.floor(n%864e5/36e5)}h ${Math.floor(n%36e5/6e4)}m ${Math.floor(n%6e4/1e3)}s`,n<0&&(clearInterval(t),document.getElementById("contador").innerHTML="Meu Aniversário de 16 anos chegou!!!")},1e3);
